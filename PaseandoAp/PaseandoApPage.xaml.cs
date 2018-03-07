bdb2e8765aa2cfb8b6f438ad7ae3372251ff379f
XpertMarketing1 <xpertmarketing@icloud.com>
2018-03-07 17:35:27 -0600

    Creacion de carpetas Iniciales

---
+++ PaseandoAp/PaseandoApPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PaseandoAp
+{
+    public partial class PaseandoApPage : ContentPage
+    {
+        public PaseandoApPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
