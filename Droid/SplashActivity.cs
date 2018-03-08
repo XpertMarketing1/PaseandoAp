d9d819c01c45d2c17c418fb51cfc62cbb6da721d
XpertMarketing1 <xpertmarketing@icloud.com>
2018-03-07 20:12:37 -0600

    Splash Screen Funcionando en cuaquier dispositivo

---
+++ Droid/SplashActivity.cs
@@ -0,0 +1,16 @@
+﻿namespace PaseandoAp.Droid
+{
+    using Android.App;
+    using Android.OS;
+
+    [Activity (Theme ="@style/Theme.Splash",MainLauncher=true,NoHistory=true)]
+    public class SplashActivity : Activity
+    {
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+            base.OnCreate(savedInstanceState);
+            System.Threading.Thread.Sleep(400);
+            this.StartActivity(typeof(MainActivity));
+		}
+	}
+}
